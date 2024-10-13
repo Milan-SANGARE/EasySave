f52195bbd2d9545f7cc860e3be0b910a23981a38
SANGARE Milan <sangare.milan.info@gmail.com>
2024-10-13 23:34:37 +0200

    version 1.0

---
+++ livrable1/Program.cs
@@ -0,0 +1,16 @@
+﻿using EasySave.NS_ViewModel;
+
+namespace EasySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Init Program
+            ViewModel viewModel = new ViewModel();
+
+        // Run Program
+        viewModel.Run();
+        }
+    }
+}
